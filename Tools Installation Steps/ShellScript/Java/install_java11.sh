6bda3effb821160f0830f4dcad3cb4572de86afe
Moole Muralidhara Reddy <115227391+techworldwithmurali@users.noreply.github.com>
2024-11-02 05:49:09 +0530

    Update install_java11.sh

--- Tools Installation Steps/ShellScript/Java/install_java11.sh
+++ Tools Installation Steps/ShellScript/Java/install_java11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Step 1: Install Java 11 on Amazon Linux 2023
+# Step 1: Install Java 11 on Amazon Linux
 echo "Installing Java 11..."
 sudo yum install -y java-11-amazon-corretto-devel
 
